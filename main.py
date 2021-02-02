38fba8030a9961227deacda97692df39df47b224
lorian-fate <ta01001011@gmail.com>
2021-02-02 12:21:06 +0100

    third final commit

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ def main():
     emp3 = Employee('Cargan','Vex','M-101011-O','car@gmail.com','carGan',40000,'10AM')
     emp4 = Employee('Della','Sart','F-010102-M','della@gmail.com','deSart',60000,'10PM')
     emp5 = Employee('Ember','War','F-010103-M','ember@gmail.com','wArem',20000,'10AM')
+    emp6 = Employee('Eternity','Vargard','F-010104-M','bart@gmail.com','AkirIdIOn',30000,'11AM')
+    emp7 = Employee('Barbatos','Vex','M-101012-O','true@gmail.com','VeritY',39000,'09AM')
+    emp8 = Employee('Drax','Berigan','M-101013-O','berg@gmail.com','Eternium',55000,'11AM')
+    emp9 = Employee('Kalaella','davos','F-010105-M','davos@gmail.com','deRitio',60000,'14PM')
+    emp10 = Employee('Balika','Versus','F-010106-M','vers@gmail.com','Warrior',25000,'15AM')
 
-    obj = Department('HHRR', '917000222',[emp1,emp2,emp3,emp4,emp5])
+    obj = Department('HHRR', '917000222',[emp1,emp2,emp3,emp4,emp5,emp6,emp7,emp8,emp9,emp10])
+    
 
     return obj.employee_report()
 
--- package/department.py
+++ package/department.py
@@ -7,37 +7,35 @@
 from .employee import Employee
 
 class Department:
-    list_employee = []
-    
-    def __init__(self, name, telephone_number, employee_list):
+    employee_dic = {}
+    def __init__(self, name, telephone_number, employee_list, employe==None):
         self.name = name
         self.telephone_number = telephone_number
         self.employee_list = employee_list
+        for i in self.employee_list:
+            self.employee_dic[i.__dict__['name']] = i.__dict__['salary']
+
+            
     
     def average_salary(self):
-        employee_dic = {}
-        for i in self.employee_list:
-            employee_dic[i.__dict__['name']] = i.__dict__['salary']
-        salary_list = list(employee_dic.values())
+        salary_list = list(self.employee_dic.values())
         length = len(salary_list)
         average = sum(salary_list) / length
         return f"The mean salary is {average}"
 
     def employee_report(self):
-        employee_dic = {}
-        for i in self.employee_list:
-            employee_dic[i.__dict__['name']] = i.__dict__['salary']
-        
-        salary = list(employee_dic.values())
+        salary = list(self.employee_dic.values())
         salary.sort()
+        salary.reverse()
         print('Employee', '\t', 'Salary')
+        print('.....','\t\t','....')
         for k in salary:
-            name = list(employee_dic.keys())[list(employee_dic.values()).index(k)]
+            name = list(self.employee_dic.keys())[list(self.employee_dic.values()).index(k)]
             if len(name) > 6:
                 print(name, '\t', k)
             else:
                 print(name, '\t\t', k)
-        print('============================')
+        print('======================')
         return 'this is the employee report'
         
 
@@ -50,8 +48,13 @@ emp2 = Employee('Beast','Gard','M-101010-O','beas@gmail.com','bestCard',30000,'0
 emp3 = Employee('Cargan','Vex','M-101011-O','car@gmail.com','carGan',40000,'10AM')
 emp4 = Employee('Della','Sart','F-010102-M','della@gmail.com','deSart',60000,'10PM')
 emp5 = Employee('Ember','War','F-010103-M','ember@gmail.com','wArem',20000,'10AM')
+emp6 = Employee('Eternity','Vargard','F-010104-M','bart@gmail.com','AkirIdIOn',30000,'11AM')
+emp7 = Employee('Barbatos','Vex','M-101012-O','true@gmail.com','VeritY',39000,'09AM')
+emp8 = Employee('Drax','Berigan','M-101013-O','berg@gmail.com','Eternium',55000,'11AM')
+emp9 = Employee('Kalaella','davos','F-010105-M','davos@gmail.com','deRitio',60000,'14PM')
+emp10 = Employee('Balika','Versus','F-010106-M','vers@gmail.com','Warrior',25000,'15AM')
 
-obj = Department('HHRR', '917000222',[emp1,emp2,emp3,emp4,emp5])
+obj = Department('HHRR', '917000222',[emp1,emp2,emp3,emp4,emp5,emp6,emp7,emp8,emp9,emp10])
 
 print(obj.average_salary())
 print(obj.employee_report())
