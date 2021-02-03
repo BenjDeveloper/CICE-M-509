ee283cf69dff98c87c9dc0b7a38107539a6e1222
GuillermoGinestal <willyg.ginestal@gmail.com>
2021-02-03 11:48:53 +0100

    commit: ejercicio 8 y 9

--- package/departamento.py
+++ package/departamento.py
@@ -3,14 +3,46 @@
 #     nombre:str
 #     telefono:str
 
+# 8.- Edite la clase Departamento y agrege una lista de Empleados para poder manejar los empleados 
+#     de cada Departamento
 
-class Departamento():
+# 9.- Edite la clase Departamento y agregele metodos para el calculo de la media salarial 
+#     del Departamento, tambien un metodo que imprima un reporte de los empleados y sus salarios,
+#     ordenados de mayor a menor en funcion de sus salarios
+
+from empleado import Empleado
+class Departamento:
+    e1 = Empleado(1400,'L-V 8H a 18H','raulito@gmail.com','asafsfafsf','Raúl','González','González', '04358213-Z', 'Calle Piedra 28')
+    e2 = Empleado(1800,'L-S 9H a 19H','jose@gmail.com','afhsdjfsdf','Jose','Galardo','G', '04358213-Z', 'Calle Piedra 28')
+    lista_empleados = [e1, e2]
+
     def __init__(self, departamento_nombre:str, departamento_telefono:str):
-
         self.departamento_nombre = departamento_nombre
         self.departamento_telefono = departamento_telefono
+
+    def media_salarial(self):
+        lista_salario = []
+        for salario in self.lista_empleados:
+            lista_salario.append(salario.__dict__['salario'])
+        suma_salarial = sum(lista_salario) / len(lista_salario)
+        return f"La media salarial es: {suma_salarial} euros"
+
+    def reporte_empleados(self):
+        dic_nombre_salario = {}
+        for nombre in self.lista_empleados:
+            dic_nombre_salario[nombre.__dict__['nombre']] = nombre.__dict__['salario']
+        lista_salario = list(dic_nombre_salario.values())
+        lista_salario.sort()
+        lista_salario.reverse()
+        #for x in lista_salario:
+        for nombre,salario in dic_nombre_salario.items():
+            print(nombre, salario)
+        lista_nombres = list(dic_nombre_salario.keys())
+        
+
     def __str__(self):
         return f'\n Nombre del departamento: {self.departamento_nombre}\n Teléfono del departamento: {self.departamento_telefono}'
 
 departamento = Departamento('Desarrollo','916374567')
-print(departamento)
+print(departamento.media_salarial())
+print(departamento.reporte_empleados())
--- package/empleado.py
+++ package/empleado.py
@@ -7,14 +7,15 @@ from usuario import Usuario
 
 class Empleado(Persona, Usuario):
     
-    def __init__(self, salario:float, horario:str, email, contraseña, nombre:str, apellido:str, fecha_nacimiento:str, dni:str, direccion:list):
+    def __init__(self, salario:float, horario:str, email, contraseña, nombre:str, apellido:str, fecha_nacimiento, dni:str, direccion:list):
         Usuario.__init__(self,  email, contraseña)
         Persona.__init__(self,nombre, apellido, fecha_nacimiento, dni, direccion)
         self.salario = salario
         self.horario = horario
 
     def __str__(self):
-        return f'\n Nombre: {self.nombre}\n Apellido:{self.apellido}\n DNI: {self.dni}\n Dirección: {self.direccion}\n Horario: {self.horario}\n Email: {self.email }\n Contraseña: {self.contraseña }\n Salario: {self.salario }'
+        return f'\n Nombre: {self.nombre}\n Apellido:{self.apellido}\n DNI: {self.dni}\
+            \n Dirección: {self.direccion}\n Horario: {self.horario}\n Email: {self.email }\n Contraseña: {self.contraseña }\n Salario: {self.salario }'
 
-e1 = Empleado(1400,'L-V 8H a 18H','raulito@gmail.com','asafsfafsf','Raúl','González','González', '04358213-Z', 'Calle Piedra 28')
+e1 = Empleado(1400,'L-V 8H a 18H','raulito@gmail.com','asafsfafsf','Raúl','González','10/02/2000', '04358213-Z', 'Calle Piedra 28')
 print('Información del empleado:\n',e1)
