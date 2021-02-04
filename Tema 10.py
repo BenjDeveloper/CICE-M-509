7e6bb9e42d178f3fe702f27bb08b50cd764c5c62
PBarnett24 <pbarneto25@gmail.com>
2021-02-04 11:58:37 +0100

    commit

--- Tema 10.py
+++ Tema 10.py
@@ -103,11 +103,57 @@ def p(*args):
 
 #!EXCLUSION DE GRUPOS
 
-texto = 'hlo hola hoola hooola hoooooooola hula hila'
+# texto = 'hlo hola hoola hooola hoooooooola hula hila'
 
-def buscar (patrones, texto):
-    for patron in patrones:
-        print(re.findall(patron, texto))
+# def buscar (patrones, texto):
+#     for patron in patrones:
+#         print(re.findall(patron, texto))
 
-patrones = ['h[o]la', 'h[^o]la']
-buscar(patrones , texto)
+# patrones = ['h[o]la', 'h[^o]la']
+# buscar(patrones , texto)
+
+
+# class MyClass():
+#     def foo(self):
+#         p('foo')
+#     def estatico(x):
+#         p(x)
+
+# obj = MyClass()
+# obj.foo()
+
+# MyClass.estatico('no soy foo soy estatico')
+
+#! INTERPOLAR VARIABLES DENTRO DE CADENAS
+
+# p('hola me llamo {v1}'.format (v1 = 'Pablo'))
+
+# v1 = 'Jajuan'
+# p(f'Hola me llamo {v1}')
+
+#! REDONDEO
+
+# p(round(4.86))
+# p(round(4.34))
+# p(round(4.51))
+
+# x = 3.14159265
+
+# for i in range(2,11):
+#     p(round(x,i))
+
+#! FLOOR CEIL 
+
+# import math
+
+# x = 1.1
+# p(x, ' -ceil-', math.ceil(x))
+# p(x, ' -floor-', math.floor(x))
+
+#! NUMEROS ALEATORIOS
+
+import random
+
+p(random.randint(10,20))
+for i in range(3):
+    p(random.randrange(10,100,2))
