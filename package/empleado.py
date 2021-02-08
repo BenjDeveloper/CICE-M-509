b9131be989ca8dc7b8da0d4984b738dd9dfa6fb4
javi <javimenendezg@gmail.com>
2021-02-08 11:14:42 +0100

    commit: whit datetime

--- package/empleado.py
+++ package/empleado.py
@@ -1,4 +1,5 @@
 
+from datetime import time
 from .Persona import Persona
 from .departamento import Departamento
 from .User import User
@@ -13,7 +14,13 @@ class Empleado(Persona,User):
         
         self.salario=salario
         self.horario=horario
+        self.diainicio=horario.split(" ")[0].split("-")[0]
+        self.diafinal=horario.split(" ")[0].split("-")[1]
+        self.horainicio=time(hour=int(horario.split(" ")[1].split("-")[0][:-1]))
+        self.horafinal=time(hour=int(horario.split(" ")[1].split("-")[1][:-1]))
 
     def __str__(self):
         return f"Persona:\n{Persona.__str__(self)}\nUsuaro:\n{User.__str__(self)}\nempleado:\nsalario: {self.salario}, horario: {self.horario}"
 
+    def getHorasalDia(self):
+        return self.horafinal.hour-self.horainicio.hour
