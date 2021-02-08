83805d32ce4a3cbe14b5ec9216d5f5f2c28e9692
“VeronicaSH” <veronicasalguero.95@gmail.com>
2021-02-08 10:10:06 +0100

    Update UNIDAD10.py

--- UNIDAD10.py
+++ UNIDAD10.py
@@ -54,9 +54,55 @@ gmail == '[A-z]'{2,10}
 . == '[.]'
 com == ['A-z0-9']{2,4}'''
 
-texto = 'vsalgueroherrera@gmail.com'
+'''texto = 'vsalgueroherrera@gmail.com'
 patron = '[A-z0-9]{6,30}[@][A-z]{2,10}[.][A-z0-9]{2,4}'
 if re.match(patron,texto):
     print('correo valido')
 else:
-    print('correo no valido ')
+    print('correo no valido ')'''
+
+'''#CHOICE
+
+import random
+
+nombres = ['benja','isa','evander','vero','patri','javi','nicole']
+
+for i in range(3):
+    print(random.choice(nombres))
+
+#SHUFFLE
+
+baraja = [1,2,3,4,5,6,7,8,9]
+random.shuffle (baraja)
+print(baraja)
+
+ #SAMPLE 
+
+baraja = [1,2,3,4,5,6,7,8,9]
+x = random.sample (baraja,5)
+print(x)'''
+
+#DATE TIME
+
+'''import datetime
+
+#fecha actual con hora 
+objeto_tiempo = datetime.datetime.now()
+print(objeto_tiempo)
+
+#fecha actual sin hora 
+objeto_tiempo = datetime.date.today()
+print(objeto_tiempo)'''
+
+from datetime import time 
+
+#tiempo (hora,min,seg  )
+a = time()
+print ( 'a =',a)
+
+b = time(11,54,56)
+print(b)
+
+
+
+
