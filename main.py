b5ec7035174e054bc24c3580ab4ca6045c81bc7d
Benjamin Peñaloza <bvpenaloza.11@gmail.com>
2021-02-11 00:07:04 +0100

    commit: LTS

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from package.departamento import Departamento
 import csv
 
 def main():
-
     path = '/Users/benjaminpenaloza/Dropbox/Mi Mac (MacBook Pro de Benjamin)/Documents/MyWorkspace/CICE-M-509'
     fichero = open( path+'/fichero.csv', 'r')
 
     lectura = csv.reader(fichero) 
     lista = [] 
     for fila in lectura:
+        print(fila)
         empleado = Empleado( fila[0], fila[1], fila[2], fila[3], fila[4], fila[5], fila[6], fila[7], fila[8], fila[9])
         lista.append(empleado)
 
@@ -24,71 +24,6 @@ def main():
 
     fichero.close()
 
-
-
-
 main()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#media_salarial (empleados.departamento_calidad)
-
-# peliador = Peliador('Canelo', 80, 30, '12634784') # instancia de Peliador
-# temporal = Temporal()
-# coach =  Entrenador('Reinoso', '533564232')
-# print (peliador)
-# print('ANTES DE LA ASIGNACION')
-# print(peliador.entrenador)
-# print(temporal.entrenador)
-
-# peliador.entrenador = coach
-# temporal.entrenador = coach
-
-# print('DESPUES DE LA ASIGNACION')
-# print(peliador.entrenador)
-# print(temporal.entrenador)
-
-# # coach.nombre = 'envander'             # edicion del objeto desde el mismo
-# #temporal.entrenador.nombre = 'Nicole'  # edicion del objeto desde una referencia - instancia ( temporal )
-# peliador.entrenador.nombre = 'Veronica' # edicion del objeto desde una referencia - instancia ( peliador )
-
-# print('DESPUES DE LA EDICION DE NOMBRE')
-# print(peliador.entrenador)
-# print(temporal.entrenador)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#?File "/Users/patricia/Desktop/practicas_cice/CICE-M-509/main.py", line 4, in <module>
-    #?from persona import Persona
-#?ModuleNotFoundError: No module named 'persona'
 
