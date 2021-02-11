dfd78e02c84f3b469e101661de353a8f410539b3
“VeronicaSH” <veronicasalguero.95@gmail.com>
2021-02-11 10:48:34 +0100

    10:48

--- main.py
+++ main.py
@@ -21,18 +21,18 @@ def main():
         empleado = Empleado( fila[0], fila[1], fila[2], fila[3], fila[4], fila[5], fila[6], fila[7], fila[8], fila[9])
         lista.append(empleado)
 
-    '''#ESCRITURA DEL ARCHIVO 
+    #ESCRITURA DEL ARCHIVO 
     fichero = open( path+'/fichero.csv', 'a')
-    fichero.write(f'Patricia,Herresanchez,13-08-1993,36476888S,Segovia,patrihf@gmail.com,44raton55,True,1401.5,8:00-15:30')'''
+    fichero.write(f'Patricia,Herresanchez,13-08-1993,36476888S,Segovia,patrihf@gmail.com,44raton55,True,1401.5,8:00-15:30')
     
 
     
     
-    #EMPLEADOS Y DEPARTAMENTOS
+    '''#EMPLEADOS Y DEPARTAMENTOS
     departamento_programacion = Departamento('Programación Python', '911652637', [emp0,emp1,emp2,emp3,emp4,emp5,emp6,emp7,emp8])
 
     print(f'Media: {departamento_programacion.media_salarial():.2f}\n')
-    print(f'Reporte salarial del departamento "{departamento_programacion.nombre}" ordenado de mayor a menor salario:\n{departamento_programacion.reporte_salarios()}')
+    print(f'Reporte salarial del departamento "{departamento_programacion.nombre}" ordenado de mayor a menor salario:\n{departamento_programacion.reporte_salarios()}')'''
 
     fichero.close()
 
---
+++ package/gerencia.py
@@ -0,0 +1,6 @@
+
+class Gerencia():
+
+    #CONSTRUCTOR
+    def __init__(self, nombre_empresa):
+        self.nombre_empresa = nombre_empresa
