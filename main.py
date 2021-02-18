58921668d299ed6c8f378faa515876dbe80db86f
Benjamin Peñaloza <bvpenaloza.11@gmail.com>
2021-02-18 10:12:48 +0100

    commit: 3 puntos en el examen

--- main.py
+++ main.py
@@ -35,46 +35,104 @@ def guardaEmpleadoInput(path, name_file):
     
     fichero.close()
 
-def cargarEmpleados(path, name_file):
+
+def ver_informacion(dic_departamentos):
+    print()
+    for nombre_depart, depar in dic_departamentos.items():
+        print(nombre_depart)
+        for emple in depar.empleados:
+            print(nombre_depart,emple)
+    print()
+
+
+
+
+
+def cargar_departamenos(path, name_file):
     fichero = open( path + name_file, 'r')
 
     lista_filas_CSV = csv.reader(fichero) 
 
-    dic_empleados = {}
-    tag_departamenos = ['calidad', 'RRHH', 'prevencion', 'IT']
-    for tag in tag_departamenos:
-        dic_empleados[tag]=[]
+    dic_departamentos = {} 
+
+    for fila in lista_filas_CSV:
+        nombre_depart = fila[0]
+        departamento = Departamento( fila[0], fila[1])
+        dic_departamentos[nombre_depart] = departamento
+
+    fichero.close()
+    return dic_departamentos   
+
+
+def cargar_empleados(path, file_empleados, file_departamentos):
+    fichero = open( path + file_empleados, 'r')
+    lista_filas_CSV = csv.reader(fichero) 
+
+    dic_departamentos = cargar_departamenos(path,file_departamentos)
     
     for fila in lista_filas_CSV:
-
         nombre_depart = fila[10]
         empleado = Empleado( fila[0], fila[1], fila[2], fila[3], fila[4], fila[5], fila[6], fila[7], fila[8], fila[9])
-        dic_empleados[nombre_depart].append(empleado)
+        dic_departamentos[nombre_depart].empleados.append(empleado)
 
-    print()
-    for key, lista in dic_empleados.items():
-        for ele in lista:
-            print(key,ele)
-    print()
-    
+    ver_informacion(dic_departamentos)
+
     fichero.close()
-    return dic_empleados
+    return dic_departamentos
 
 def main():
     path = '/Users/benjaminpenaloza/Dropbox/Mi Mac (MacBook Pro de Benjamin)/Documents/MyWorkspace/CICE-M-509'
-
+    file_empleados = '/fichero_empleados.csv'
+    file_departamentos = '/fichero_departamentos.csv'
+    
     #! EMPLEADOS
-    name_file = '/fichero.csv'
-    # guardaEmpleadoInput(path, name_file)
-    dic_empleados = cargarEmpleados(path, name_file) 
-
-    #! DEPARTAMENTOS
-    # departamento_calidad = Departamento ('Calidad', '987654321', lista_empleados)
-
-    #! GERENCIA
-    # gerencia_corte_ingles = Gerencia('Corte Ingles S.A.')
-    # gerencia_corte_ingles.lista_departametos.append( departamento_calidad )
+    dic_departamentos = cargar_empleados(path, file_empleados, file_departamentos) 
 
 main()
+
+
+
+
+#! PARTE 1
+# {
+#     'calidad': [<package.empleado.Empleado object at 0x7fa3fc239ca0>], 
+#     'RRHH': [<package.empleado.Empleado object at 0x7fa3fc238370>], 
+#     'prevencion': [<package.empleado.Empleado object at 0x7fa3fc238610>], 
+#     'IT': [<package.empleado.Empleado object at 0x7fa3fc2385e0>]
+# }
+
+#! PARTE 2
+# {
+#     'calidad': [<package.empleado.Empleado object at 0x7fc437a2fca0>], 
+#     'RRHH': [<package.empleado.Empleado object at 0x7fc437a2e370>], 
+#     'prevencion': [<package.empleado.Empleado object at 0x7fc437a2e610>], 
+#     'IT':   [<package.empleado.Empleado object at 0x7fc437a2e5e0>, 
+#             <package.empleado.Empleado object at 0x7fc437a2e640>, 
+#             <package.empleado.Empleado object at 0x7fc437a2d670>]
+# }
+
+#! PARTE 3 - ESTRUCTURA AUXILIAR PARA ORDENAR LOS EMPLEADOS SEGUN SU DEPARTAMENTO
+# {
+#     'calidad': [], 
+#     'RRHH': [], 
+#     'prevencion': [], 
+#     'IT':   []
+# }
+
+#! PARTE 4
+# Gerencia
+#     nombre_empresa : str
+#     lista_departamentos :   {
+#                                 Departamento.nombre : Departamento
+#                                                                 nombre:str  
+#                                                                 telefono:str
+#                                                                 lista_empleados:[e1,e2,e3...en]
+
+#                                 'calidad' : Departamento_1 - objectos
+#                                 'RRHH': Departamento:_2, 
+#                                 'prevencion': Departamento_3, 
+#                                 'IT':   Departamento_4
+#                             }
+
 
 
