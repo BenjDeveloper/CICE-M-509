fe58f7213222f3a76f9a55e7f7d0e7f690c12b27
PBarnett24 <pbarneto25@gmail.com>
2021-04-06 12:25:05 +0200

    06/04 2

--- COVID-19/main.py
+++ COVID-19/main.py
@@ -1,19 +1,81 @@
 import requests as req
 import json
 
-response = req.get('https://datos.comunidad.madrid/catalogo/dataset/7da43feb-8d4d-47e0-abd5-3d022d29d09e/resource/ead67556-7e7d-45ee-9ae5-68765e1ebf7a/download/covid19_tia_muni_y_distritos.json').json()
+# response = req.get('https://datos.comunidad.madrid/catalogo/dataset/7da43feb-8d4d-47e0-abd5-3d022d29d09e/resource/ead67556-7e7d-45ee-9ae5-68765e1ebf7a/download/covid19_tia_muni_y_distritos.json').json()
 
 # print(response.content)
 
-with open("COVID-19/fichero.json", 'w') as file:
-    json.dump(response, file)
+# with open("COVID-19/fichero.json", 'w') as file:
+#     json.dump(response, file)
+
+def total_municipality():
+    with open('COVID-19/fichero.json', 'r') as data:    
+            lectura = json.load(data)
+            print(lectura['data'][0])
+
+            contador = 0
+            for municipio in lectura['data']:
+                if municipio['municipio_distrito'] == 'Valdemorillo':
+                    contador += 1
+            print(f'El total de municipios es: {25237/contador:.0f}')
+
+def get_tia_final():
+    with open('COVID-19/fichero.json', 'r') as data:    
+        lectura = json.load(data)
+        lista_casos = []
+        # contador = 0
+        for municipio in lectura['data'][:199]:
+            # if contador <= 198:
+            try:
+                assert 'casos_confirmados_totales' in list(municipio.keys())
+            except AssertionError:
+                pass
+            else:
+                lista_casos.append(municipio['casos_confirmados_totales'])
+                # contador += 1
+        print(f'La TIA final es: {sum(lista_casos)}')
 
 
 
-def cantidad_municipios():
+def get_tia_inicial():
+    with open('COVID-19/fichero.json', 'r') as data:    
+        lectura = json.load(data)
+        lista_casos = []
+        # contador = 0
+        for municipio in lectura['data'][-200:-1]:
+            # if contador <= 198:
+            try:
+                assert 'casos_confirmados_totales' in list(municipio.keys())
+            except AssertionError:
+                pass
+            else:
+                lista_casos.append(municipio['casos_confirmados_totales'])
+                # contador += 1
+                # total = sum(lista_casos)
+        print(f'La TIA inicial es: {sum(lista_casos)}')
 
-    lista_municipios = []
-    for element in response:
-        lista_municipios.append(element['municipio_distrito'])
+def fecha():
+    with open('COVID-19/fichero.json', 'r') as data:    
+        lectura = json.load(data)
+        lista_fecha = []
+        contador = 0
+        for municipio in lectura['data']:
+            if municipio['fecha_informe'] == f"2020/02/{26} 07:00:00":
+                contador += 1
+                lista_fecha.append(contador)
 
-print(cantidad_municipios)  
+# def tia_diaria():
+#     with open('COVID-19/fichero.json', 'r') as data:    
+#         lectura = json.load(data)
+#         lista_tia_diaria = []
+
+
+total_municipality()
+get_tia_inicial()
+get_tia_final()
+
+
+
+
+
+
