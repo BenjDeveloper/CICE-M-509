39fe1042cbf3a8143a02f3c32e2834f212d074e6
PBarnett24 <pbarneto25@gmail.com>
2021-04-08 12:20:11 +0200

    08/04

---
+++ ejerciciovitoestadistica/main_1.py
@@ -0,0 +1,54 @@
+import requests as req
+import json
+response = req.get("https://datos.comunidad.madrid/catalogo/dataset/7da43feb-8d4d-47e0-abd5-3d022d29d09e/resource/ead67556-7e7d-45ee-9ae5-68765e1ebf7a/download/covid19_tia_muni_y_distritos.json").json()
+with open("covid.json", "w") as file:
+    json.dump(response, file)
+import std
+#import matplotlib.pyplot as plt
+
+with open("covid.json") as file:
+    json_reader = json.load(file)
+    data = json_reader["data"]
+    # result = set([mun["municipio_distrito"] for mun in data]) # 199
+    def get_tia(parse):
+        result = 0
+        for mun in parse:
+            try:
+                result += mun["casos_confirmados_totales"]
+            except KeyError:
+                result += 0
+        return result
+    # first_tia = get_tia(data[-200:-1])
+    # last_tia = get_tia(data[0:199])
+    # print(last_tia)
+    def get_all(dataset):
+        result = []
+        dates = sorted(set([date['fecha_informe']for date in data])) #sorted ---> ordena los elementos de una lista, set..
+        for date in dates:
+            inner = 0
+            for mun in dataset:
+                if mun['fecha_informe'] == date:
+                    try:
+                        inner += mun['casos_confirmados_totales']
+                    except KeyError:
+                        inner += 0
+            result.append(inner)
+        return result
+        
+    y = get_all(data)
+    #y.reverse()
+    # print(len(y))
+    
+    x = [num for num in range(1,128)]
+    #print(x)
+    #print(y)
+
+    test = std.Statistics(x, y)
+    #print(test.y[0])
+    print(test.get_prediction(150) - 15000)
+    print("r Pearson --> ", test.r_pearson)
+
+    
+    #plt.plot(x,y)
+    #plt.ylabel(y)
+    #plt.show()
---
+++ ejerciciovitoestadistica/plots.py
@@ -0,0 +1,12 @@
+from main_1 import x, y
+import matplotlib.pyplot as plt
+
+x1 = [10,20,30,40,50,60,70]
+y1 = [10000,20000,30000,40000,50000,60000,70000]
+
+plt.plot(x,y,'g-',label='COVID-19',)
+plt.ylabel('Casos confirmados')
+plt.xlabel('Dias')
+plt.plot(x1,y1,'m',label='Prueba')
+plt.legend()
+plt.show()
