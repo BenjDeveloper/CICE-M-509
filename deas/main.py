426ba326e68a84d92d26af937da26139ca9108e6
PBarnett24 <pbarneto25@gmail.com>
2021-04-26 11:53:44 +0200

    26/04  2

--- deas/main.py
+++ deas/main.py
@@ -1,11 +1,11 @@
 import requests as req
 import json
 response = req.get("https://datos.comunidad.madrid/catalogo/dataset/35609dd5-9430-4d2e-8198-3eeb277e5282/resource/c38446ec-ace1-4d22-942f-5cc4979d19ed/download/desfibriladores_externos_fuera_ambito_sanitario.json").json()
-with open("crud/desfibriladores.json", "w") as file:
+with open("deas/desfibriladores.json", "w") as file:
     json.dump(response, file, indent=4)
 
 def get_data():
-    with open ('crud/desfibriladores.json') as file:
+    with open ('deas/desfibriladores.json') as file:
         json_reader = json.load(file)
         data = json_reader["data"]
         return data
@@ -15,9 +15,9 @@ data = get_data()
 public_dea_number = list(map(lambda des:des["tipo_titularidad"] == 'Pública', data))
 private_dea_number = list(map(lambda des:des["tipo_titularidad"] == 'Privada', data))
 
-print(sum(public_dea_number))
-print(sum(private_dea_number))
-print(sum(public_dea_number + private_dea_number))
+# print(sum(public_dea_number))
+# print(sum(private_dea_number))
+# print(sum(public_dea_number + private_dea_number))
 
 
 def m30_dea_number(given):
@@ -27,4 +27,30 @@ def m30_dea_number(given):
         counter += 1 if dea["direccion_codigo_postal"] in postal_code_m30 else 0
     return counter
 a = m30_dea_number(data)
-print(a)
+# print(a)
+
+def menu():
+    print("DEAS")
+    print('Opcion 1: Crear usuario')
+    print('Salir')
+menu()
+user = input('Elija opcion: ')
+
+while user.lower() != 'Salir':
+    if user == '1':
+        name = input('Introduzca su nombre: ')
+        password = input('Introduzca su contraseña: ')
+        new_user = {'Nombre': name, 'Contraseña': password}
+        def opcion_1():
+            with open ('deas/users.json', 'r') as file:
+                users = json.load(file)
+                return users
+        users = opcion_1()
+        users['data'].append(new_user)
+        with open('deas/users.json') as file:
+            json.dump(users, file)
+        menu()
+        user = input(': ')
+
+        
+        
--- deas/models.py
+++ deas/models.py
@@ -1,4 +1,18 @@
 class Dea:
-    def __init__(self, x, y, id)
+    def __init__(self, x, y, dea_id):
     self.x = x
     self.y = y
+    self.dea_id = dea_id
+
+    def get_distance(self, user_x, user_y, 10):
+        c_1 = abs(user_x) + abs(self.x)**2
+        c_2 = abs(user_y) + abs(self.y)**2
+        return (c_1+c_2)**0.5
+
+    
+class User:
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = x 
+        self.y = y 
+        
